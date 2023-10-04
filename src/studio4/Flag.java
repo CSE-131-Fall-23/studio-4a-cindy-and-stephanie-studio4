250641a713f93f894a24316f88fba080ef1d2515
TheStormfly <TheStormfly@localhost>
2023-10-04 15:47:11 -0500

    I made a flag

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,16 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.clear();
+		StdDraw.setPenColor(102, 102, 255);
+		StdDraw.filledRectangle(0, 0, .3, .3);
+		StdDraw.filledRectangle(0, .7, .3, .3);
+		StdDraw.filledRectangle(.7, 0, .3, .3);
+		StdDraw.filledRectangle(.7, .7, .3, .3);
+		StdDraw.setPenColor(255, 153, 255);
+		StdDraw.ellipse(.71, .68, .07, .07);
+		StdDraw.filledEllipse(.74, .7, .01, .01);
+		StdDraw.filledEllipse(.68, .7, .01, .01);
+		StdDraw.arc(.71, .67, .04, 180, 0);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,6 +1,7 @@
 package studio4;
 
 import java.io.File;
+
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
@@ -9,9 +10,6 @@ import javax.swing.SwingUtilities;
 
 import edu.princeton.cs.introcs.StdDraw;
 
-/**
- * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
- */
 public class InterpretDrawingFile {
 
 	public static void main(String[] args) throws FileNotFoundException {
@@ -19,6 +17,34 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+
+		String shapeType = in.next();
+		int red = in.nextInt();
+		int green= in.nextInt();
+		int blue = in.nextInt();
+		boolean isFilled= in.nextBoolean();
+		double x1= in.nextDouble();
+		double y1= in.nextDouble();
+		double x2= in.nextDouble();
+		double y2= in.nextDouble();
+
+		StdDraw.clear();
+		StdDraw.setPenColor(red, green, blue);
+		if (shapeType .equals("rectangle"))
+		{
+			StdDraw.filledRectangle(x1, y1, x2, y2);
+		}
+		if (shapeType .equals("ellipse"))
+		{
+			StdDraw.filledEllipse(x1, y1, x2, y2);
+		}
+		if (shapeType .equals("triangle"))
+		{
+			double x3 = in.nextDouble();
+			double y3 = in.nextDouble();
+			double Xarray [] = {x1, x2, x3};
+			double Yarray [] = {y1, y2, y3};
+			StdDraw.polygon(Xarray, Yarray);
+		}
 	}
 }
